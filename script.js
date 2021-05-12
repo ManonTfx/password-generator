ced98cdcf2c58cb262e69ffee8bb35cfb1e4bdb5
ManonTfx <manon.trefoux@gmail.com>
2021-05-12 10:44:19 +0200

    add const for data

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
 const passwordOutput = document.getElementById('passwordOutput');
 
+const dataLowercase = "azertyuiopqsdfghjklmwxcvbn".split('');
+const dataUppercase = "AZERTYUIOPQSDFGHJKLMWXCVBN".split('');
+const dataNumbers = "123456789".split('');
+const dataSymbols = "~!@#$%^&*-_=+;:,.<>/?|".split('');
+
+
+
